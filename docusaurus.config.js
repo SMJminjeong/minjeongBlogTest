1f6d1bb34d5925570775abf64c6ed33fc4a19aa4
SMJminjeong <sdwp0920@gmail.com>
2023-12-24 21:41:08 +0900

    :wrench: add docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 (module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://SMJminjeong.com',
+  url: 'https://SMJminjeong.github.io',
   baseUrl: '/minjeongBlog/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
